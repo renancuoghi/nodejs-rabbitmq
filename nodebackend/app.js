df0cb8d52e1792751ab8ae60f4a434146a5e2038
Renan <renan.cuoghi@gmail.com>
2022-06-02 22:05:47 -0300

    change rabbit client format

--- nodebackend/app.js
+++ nodebackend/app.js
@@ -2,20 +2,19 @@ const config = require('./config.json');
 const express = require('express');
 const Rabbitmq = require("./rabbitmq");
 
-const rabbitSender = new Rabbitmq(config.rabbitmq_url);
-const rabbitConsumer = new Rabbitmq(config.rabbitmq_url);
+const rabbit = new Rabbitmq(config.rabbitmq_url);
 
 const app = express();
 
 app.get('/', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');    
-    await rabbitSender.sendToQueue('queue1', 'Message test', (err) => res.json({'message' : err},500));
+    await rabbit.sendToQueue('queue1', 'Message test', (err) => res.json({'message' : err},500));
     res.json({'message' : 'running!!'});
 });
 
 app.get('/consume', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    await rabbitConsumer.consume('queue1',  (message) => {
+    await rabbit.consume('queue1',  (message) => {
         res.json({'message' : message.content.toString()})
     }, (err) => {res.json({'message' : err},500)   });
 });
